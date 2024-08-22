157c229328d6fa27b79d4337bb44e1ff115f35b7
DanielLai <xingdaniellaixiong@gmail.com>
2024-08-22 17:27:12 +0800

    Edited chatbot for distinctness

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Kai User Guide
 
 // Update the title above to match the actual product name
 
--- src/main/java/Kai.java
+++ src/main/java/Kai.java
@@ -1,10 +1,11 @@
 public class Kai {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "__ ___   _  _____ \n" +
+                "| |/ /  /_\\ |_ _|\n" +
+                "|   <  / _ \\ | |\n" +
+                "|_|\\_\\/_/ \\_\\___|";
+        System.out.println("Hello! I'm \n" + logo);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
