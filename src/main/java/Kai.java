8f46e4867db403da9035ad5f206cde71fac84a01
DanielLai <xingdaniellaixiong@gmail.com>
2024-08-22 18:30:21 +0800

    Added echo functionality

--- src/main/java/Kai.java
+++ src/main/java/Kai.java
@@ -1,11 +1,21 @@
+import java.util.Scanner;
+
 public class Kai {
     public static void main(String[] args) {
-        String logo = "__ ___   _  _____ \n" +
-                "| |/ /  /_\\ |_ _|\n" +
-                "|   <  / _ \\ | |\n" +
-                "|_|\\_\\/_/ \\_\\___|";
-        System.out.println("Hello! I'm \n" + logo);
-        System.out.println("What can I do for you?");
+        String logo = "\t __ ___   _  _____ \n" +
+                "\t | |/ /  /_\\ |_ _|\n" +
+                "\t |   <  / _ \\ | |\n" +
+                "\t |_|\\_\\/_/ \\_\\___|";
+        System.out.println("\t Hello! I'm \n" + logo);
+        System.out.println("\t What can I do for you?");
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) break;
+            System.out.println("\t" + input);
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
