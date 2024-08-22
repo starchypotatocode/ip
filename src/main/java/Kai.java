304a8e260c4e0a812534781b0a2bf4794aa70c38
DanielLai <xingdaniellaixiong@gmail.com>
2024-08-22 18:52:36 +0800

    Replaced echo functionality with task record/reminder system

--- src/main/java/Kai.java
+++ src/main/java/Kai.java
@@ -7,15 +7,33 @@ public class Kai {
                 "\t |   <  / _ \\ | |\n" +
                 "\t |_|\\_\\/_/ \\_\\___|";
         System.out.println("\t Hello! I'm \n" + logo);
-        System.out.println("\t What can I do for you?");
+        System.out.tprintln("\t What can I do for you?");
 
         Scanner sc = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int ptr = 0;
         while (true) {
             String input = sc.nextLine();
             if (input.equals("bye")) break;
-            System.out.println("\t" + input);
+            else if (input.equals("list")) {
+                if (ptr == 0) System.out.println("\t No tasks to list, please add some and try again?");
+                for (int i = 0; i < ptr; i++) {
+                    System.out.println("\t " + (i + 1) + ". " + tasks[i]);
+                }
+            }
+            else if (!input.isEmpty()){
+                if (ptr >= 100) {
+                    System.out.println("\t Sorry, I'm out of space for tasks");
+                    continue;
+                }
+                tasks[ptr] = input;
+                ptr++;
+                System.out.println("\t Task Added: " + input);
+            } else {
+                System.out.println("\t I would love to help you, but could you please give me more to work with?");
+            }
         }
 
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("\t Bye. Hope to see you again soon!");
     }
 }
