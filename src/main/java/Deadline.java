756c520c250eed77bd8733eb0138530cd5d5c89d
DanielLai <xingdaniellaixiong@gmail.com>
2024-08-22 20:34:42 +0800

    Added more task variants, although testing isn't very complete

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,11 @@
+public class Deadline extends Task{
+    private String deadline;
+    public Deadline(String desc, String deadline) {
+        super(desc);
+        this.deadline = deadline;
+    }
+    @Override
+    public String toString() {
+        return "[D] " + super.toString() + "(by: " + deadline + ")";
+    }
+}
--- src/main/java/Kai.java
+++ src/main/java/Kai.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Kai {
@@ -10,55 +11,65 @@ public class Kai {
         System.out.println("\t What can I do for you?");
 
         Scanner sc = new Scanner(System.in);
-        Task[] tasks = new Task[10];
-        int ptr = 0;
+        ArrayList<Task> tasks = new ArrayList<>();
         while (true) {
             String input = sc.nextLine();
 
             if (input.equals("bye")) break;
             else if (input.equals("list")) {
-                if (ptr == 0) {
+                if (tasks.isEmpty()) {
                     System.out.println("\t No tasks to list, please add some and try again?");
                 } else {
                     System.out.println("\t Here are the tasks in your list:");
                 }
-                for (int i = 0; i < ptr; i++) {
-                    System.out.println("\t " + (i + 1) + ". " + tasks[i].toString());
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println("\t " + (i + 1) + ". " + tasks.get(i).toString());
                 }
             } else if (input.startsWith("mark ")) {
                 try {
                     int id = Integer.parseInt(input.substring(5)) - 1;
-                    if (id >= ptr || id < 0) {
+                    if (id >= tasks.size() || id < 0) {
                         throw new IllegalArgumentException();
                     }
-                    tasks[id].markComplete();
+                    tasks.get(id).markComplete();
                     System.out.println("\t Nice! I've marked this task as done:");
-                    System.out.println("\t " + tasks[id].toString());
+                    System.out.println("\t \t " + tasks.get(id).toString());
                 } catch (IllegalArgumentException e) {
                     System.out.println("\t The task number specified is invalid, please retry.");
                 }
             } else if (input.startsWith("unmark ")) {
                 try {
                     int id = Integer.parseInt(input.substring(7)) - 1;
-                    if (id >= ptr || id < 0) {
+                    if (id >= tasks.size() || id < 0) {
                         throw new IllegalArgumentException();
                     }
-                    tasks[id].markIncomplete();
+                    tasks.get(id).markIncomplete();
                     System.out.println("\t Ok, I've marked this task as not done yet:");
-                    System.out.println("\t" + tasks[id].toString());
+                    System.out.println("\t \t " + tasks.get(id).toString());
                 } catch (IllegalArgumentException e) {
                     System.out.println("\t The task number specified is invalid, please retry.");
                 }
             } else if (!input.isEmpty()) {
                 try {
-                    tasks[ptr] = new Task(input);
-                    ptr++;
-                    System.out.println("\t Task Added: " + input);
-                } catch (ArrayIndexOutOfBoundsException e) {
-                    System.out.println("\t Sorry, I'm out of space for tasks");
+                    if (input.startsWith("todo ") && input.length() >= 6) {
+                        tasks.add(new ToDo(input.substring(5)));
+                    } else if (input.startsWith("deadline ")) {
+                        String deadline = input.substring(input.indexOf("/by ") + 4);
+                        tasks.add(new Deadline(input, deadline));
+                    } else if (input.startsWith("event ")) {
+                        String from = input.substring(input.indexOf("/from ") + 6, input.indexOf(" /to "));
+                        String to = input.substring(input.indexOf(" /to ") + 5);
+                        tasks.add(new Event(input, from, to));
+                    } else throw new IllegalArgumentException();
+                    System.out.println("\t Task Added:");
+                    System.out.println("\t \t " + tasks.get(tasks.size() - 1).toString());
+                    System.out.println("\t " + "There is now " + tasks.size() + " task(s) in the list.");
+                } catch (IllegalArgumentException e) {
+                    System.out.println("\t Invalid task input, please check how you formatted it and try again.");
                 }
             } else {
-                System.out.println("\t I would love to help you, but could you please give me more to work with?");
+                System.out.println("\t I would love to help you, " +
+                        "but could you please give me more to work with?");
             }
         }
 
