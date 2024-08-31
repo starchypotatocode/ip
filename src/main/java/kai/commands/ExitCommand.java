8ec445e1d2cfe7336f150aec31a6ff49829c3f60
DanielLai <xingdaniellaixiong@gmail.com>
2024-08-31 21:38:46 +0800

    Add MarkCommand.java, UnmarkCommand.java

--- src/main/java/kai/commands/ExitCommand.java
+++ src/main/java/kai/commands/ExitCommand.java
@@ -16,6 +16,6 @@ public class ExitCommand extends Command {
 
     @Override
     public void invoke() {
-        
+
     }
 }
---
+++ src/main/java/kai/commands/MarkCommand.java
@@ -0,0 +1,26 @@
+package kai.commands;
+
+import kai.tasks.Task;
+
+/**
+ * This Command marks the Task given as complete
+ * Can be modified as one-use later on if needed?
+ */
+public class MarkCommand extends Command {
+    private final Task task;
+
+    /**
+     * Constructs MarkCommand with the relevant Task
+     * it needs to be applied to
+     *
+     * @param task the Task that needs to be marked complete
+     */
+    public MarkCommand(Task task) {
+        this.task = task;
+    }
+
+    @Override
+    public void invoke() {
+        this.task.markComplete();
+    }
+}
