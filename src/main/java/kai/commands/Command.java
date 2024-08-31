bcdcc49f4d1c838993717aedaa7b68e224fb86bb
DanielLai <xingdaniellaixiong@gmail.com>
2024-08-31 21:35:25 +0800

    Add Command.java, ExitCommand.java

---
+++ src/main/java/kai/commands/Command.java
@@ -0,0 +1,13 @@
+package kai.commands;
+
+/**
+ * Abstract base for other concrete Commands to inherit from
+ * Using the Command design pattern to make things easier?
+ */
+public abstract class Command {
+
+    /**
+     * Allows the delay of command invoking to better timings
+     */
+    public abstract void invoke();
+}
---
+++ src/main/java/kai/commands/ExitCommand.java
@@ -0,0 +1,21 @@
+package kai.commands;
+
+import kai.Ui;
+
+/**
+ * This Command causes the chatbot to exit
+ */
+public class ExitCommand extends Command {
+
+    /**
+     * Constructor for ExitCommand
+     */
+    public ExitCommand() {
+
+    }
+
+    @Override
+    public void invoke() {
+        
+    }
+}
