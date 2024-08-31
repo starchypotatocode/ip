49a38fb4e873269fe1816b78f1977dcb266d8321
DanielLai <xingdaniellaixiong@gmail.com>
2024-08-31 15:59:15 +0800

    Add TaskList.java and refactor

---
+++ src/main/java/kai/TaskList.java
@@ -0,0 +1,69 @@
+package kai;
+
+import java.util.ArrayList;
+
+import kai.tasks.Task;
+
+/**
+ * For managing the list of Task in an encapsulated manner
+ */
+public class TaskList {
+    private ArrayList<Task> tasks;
+
+    /**
+     * Constructor of empty TaskList
+     */
+    public TaskList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    /**
+     * Constructor of TaskList with the given tasks
+     *
+     * @param tasks the list of tasks in the class
+     */
+    public TaskList(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    /**
+     * Add a Task to the TaskList
+     *
+     * @param task the Task to be added
+     */
+    public void add(Task task) {
+        tasks.add(task);
+    }
+
+    /**
+     * Remove a Task from the TaskList, based on the index given
+     * (Assumes the index given is valid)
+     *
+     * @param index the index of the Task to be deleted
+     */
+    public void remove(int index) {
+        tasks.remove(index);
+    }
+
+    /**
+     * Check if the TaskList is empty and return accordingly
+     *
+     * @return a boolean about if the TaskList is empty
+     */
+    public boolean isEmpty() {
+        return tasks.isEmpty();
+    }
+
+    /**
+     * To check the size of the TaskList
+     *
+     * @return the number of Tasks in the TaskList
+     */
+    public int size() {
+        return tasks.size();
+    }
+
+    public Task getTask(int index) {
+        return tasks.get(index);
+    }
+}
