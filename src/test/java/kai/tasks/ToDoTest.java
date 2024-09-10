203c7f21ff630ed923b2b847025e2e1c7245e646
DanielLai <xingdaniellaixiong@gmail.com>
2024-09-10 19:31:51 +0800

    Fix broken tests

--- src/test/java/kai/tasks/ToDoTest.java
+++ src/test/java/kai/tasks/ToDoTest.java
@@ -8,39 +8,39 @@ public class ToDoTest {
 
     @Test
     public void serialize() {
-        ToDo task = new ToDo("");
+        ToDo task = new ToDo("   ");
         ToDo temp = new ToDo("meow");
 
-        assertEquals(task.serialize(), "T | 0 | ");
+        assertEquals(task.serialize(), "T | 0 |    ");
         assertEquals(temp.serialize(), "T | 0 | meow");
 
         task.setComplete();
         temp.setComplete();
-        assertEquals(task.serialize(), "T | 1 | ");
+        assertEquals(task.serialize(), "T | 1 |    ");
         assertEquals(temp.serialize(), "T | 1 | meow");
 
         task.setIncomplete();
         temp.setIncomplete();
-        assertEquals(task.serialize(), "T | 0 | ");
+        assertEquals(task.serialize(), "T | 0 |    ");
         assertEquals(temp.serialize(), "T | 0 | meow");
     }
 
     @Test
     public void toString_all() {
-        ToDo task = new ToDo("");
+        ToDo task = new ToDo("   ");
         ToDo temp = new ToDo("meow");
 
-        assertEquals(task.toString(), "[T] [ ] ");
+        assertEquals(task.toString(), "[T] [ ]    ");
         assertEquals(temp.toString(), "[T] [ ] meow");
 
         task.setComplete();
         temp.setComplete();
-        assertEquals(task.toString(), "[T] [X] ");
+        assertEquals(task.toString(), "[T] [X]    ");
         assertEquals(temp.toString(), "[T] [X] meow");
 
         task.setIncomplete();
         temp.setIncomplete();
-        assertEquals(task.toString(), "[T] [ ] ");
+        assertEquals(task.toString(), "[T] [ ]    ");
         assertEquals(temp.toString(), "[T] [ ] meow");
     }
 }
